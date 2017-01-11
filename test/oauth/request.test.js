4332559bf972bd9297c8fe271faab1fdc5b35591
Piroro-hs <Piroro-hs@users.noreply.github.com>
2017-01-11 11:19:04 +0900

    Add tests for request

--- test/oauth/request.test.js
+++ test/oauth/request.test.js
@@ -1,52 +1,59 @@
+import {readFile} from 'fs';
+
 import test from 'ava';
 
 import fetch from 'node-fetch';
+import URLSearchParams from 'url-search-params';
 
 import request from '~/oauth/request';
 
-test.before((t) => {global.fetch = fetch;});
+let tokens;
 
-test('obtains request token', (t) => {
-  const method = 'POST';
-  const url = 'http://term.ie/oauth/example/request_token.php';
-  const tokens = {consumerKey: 'key', consumerSecret: 'secret'};
-  return request(url, {method}, tokens)
-    .then(response => response.text())
-    .then((text) => {
-      t.is(text, 'oauth_token=requestkey&oauth_token_secret=requestsecret');
+test.before.cb((t) => {
+  global.fetch = fetch;
+  readFile('test/tokens.json', 'utf8', (_, json) => {
+    const {consumerKey, consumerSecret, accessToken, accessTokenSecret} = JSON.parse(json);
+    tokens = {
+      consumerKey,
+      consumerSecret,
+      oauthToken: accessToken,
+      oauthTokenSecret: accessTokenSecret,
+    };
+    t.end();
+  });
+});
+
+test('make GET request', (t) => {
+  const method = 'GET';
+  const url = 'https://api.twitter.com/1.1/application/rate_limit_status.json';
+  const params = {resources: 'help'};
+  return request(url, {method, params}, tokens)
+    .then(response => response.json())
+    .then(({resources: {help, users}}) => {
+      t.truthy(help);
+      t.falsy(users);
     });
 });
 
-test.todo('makes POST request', /*(t) => {
+test('make POST request / handle missing tokens / handle extra oauth params', (t) => {
   const method = 'POST';
-  const url = 'http://term.ie/oauth/example/echo_api.php';
-  const body = {a: 'b'};
-  const tokens = {
-    consumerKey: 'key',
-    consumerSecret: 'secret',
-    oauthToken: 'accesskey',
-    oauthTokenSecret: 'accesssecret',
-  };
-  return request(url, {method, body}, tokens)
+  const url = 'https://api.twitter.com/oauth/request_token';
+  const body = {x_auth_access_type: 'write'};
+  return request(
+    url,
+    {method, body},
+    {consumerKey: tokens.consumerKey, consumerSecret: tokens.consumerSecret},
+    {oauth_callback: 'oob'},
+  )
     .then(response => response.text())
     .then((text) => {
-      t.is(text, 'a=b');
+      const params = new URLSearchParams(text);
+      t.true(params.has('oauth_token') && params.has('oauth_token_secret'));
     });
-}*/);
+});
 
-test('makes GET request', (t) => {
+test('parse Twitter API error messages', (t) => {
   const method = 'GET';
-  const url = 'http://term.ie/oauth/example/echo_api.php';
-  const params = {a: 'b'};
-  const tokens = {
-    consumerKey: 'key',
-    consumerSecret: 'secret',
-    oauthToken: 'accesskey',
-    oauthTokenSecret: 'accesssecret',
-  };
-  return request(url, {method, params}, tokens)
-    .then(response => response.text())
-    .then((text) => {
-      t.is(text, 'a=b');
-    });
+  const url = 'https://api.twitter.com/1.1/not/exist.json';
+  return t.throws(request(url, {method}, tokens), '34 Sorry, that page does not exist');
 });
